b43f1a094ebf99a5ce6848f648127f8992027791
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2021-03-30 20:54:24 +0200

    Added new project
    Added a project which is displaying winter holiday banner or common banner on a web-site (depending on current month).

---
+++ banner/js/main.js
@@ -0,0 +1,14 @@
+function addElements() {
+    var a = document.getElementById("hosting");
+    var m = new Date().getMonth() + 1;
+
+    if (m == 12 || m == 1 || m == 2) {
+        a.src="img/dobojcaffe.png";
+        a.alt="Web hosting by DobojCaffe.com";
+        a.className="img-fluid mx-auto d-block";
+    } else {
+        a.src="img/docaffe.jpg";
+        a.alt="Web hosting by DobojCaffe.com";
+        a.className="img-fluid mx-auto d-block";
+    }
+}
