b8cca72542e6702d4577747fcbaf573285770735
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-07-07 14:10:57 +0200

    Update BS_CP_main.js

--- private/BS_CP_main.js
+++ private/BS_CP_main.js
@@ -1,7 +1,7 @@
 $("html").attr("lang", navigator.language);
 $("head").append("<meta http-equiv='refresh' content='1800'>");
 
-$("body").on("error", function (){
+$("body").on("error", function(){
 	$("body").css({"font-family": "'Ubuntu', sans-serif", "background": "url('https://baleshevich.dobojcaffe.com/itp100/images/logo.png') no-repeat fixed left bottom"});
 })
 
