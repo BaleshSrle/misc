c869ecf512920af289a032f82025a0bd0d259f15
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-09-25 11:07:57 +0200

    Update BS_main.js

--- private/BS_main.js
+++ private/BS_main.js
@@ -15,6 +15,9 @@ $("head").append("<meta http-equiv='x-dns-prefetch-control' content='on'>");
 $("head").append("<meta http-equiv='Cache-control' content='no-cache'>");
 $("head").append("<meta http-equiv='Pragma' content='no-cache'>");
 $("head").append("<meta http-equiv='Expires' content='-1'>");
+$("head").prepend("<link rel='preconnect' href='https://fonts.googleapis.com'>");
+$("head").prepend("<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>");
+$("head").prepend("<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap' media='all'>");
 $("head").prepend("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css' crossorigin='anonymous' media='all'>");
 
 function reloadPage() {
