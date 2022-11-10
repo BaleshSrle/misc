e3ea8e6bd0235ae3242b5ddbee45b354805a5e66
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-11-10 09:01:08 +0100

    Update BS_main.js

--- private/BS_main.js
+++ private/BS_main.js
@@ -18,7 +18,7 @@ $("head").append("<meta http-equiv='Expires' content='-1'>");
 $("head").prepend("<link rel='preconnect' href='https://fonts.googleapis.com'>");
 $("head").prepend("<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>");
 $("head").prepend("<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap' media='all'>");
-$("head").prepend("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css' crossorigin='anonymous' media='all'>");
+$("head").prepend("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css' crossorigin='anonymous' media='all'>");
 
 function reloadPage() {
     location.reload();
