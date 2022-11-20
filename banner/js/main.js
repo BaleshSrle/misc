96fca57f9e8bcd3610e17fba1b69ec6584b2293c
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-11-20 14:33:29 +0100

    Changed CDN server and updated JS code to jQuery

--- banner/js/main.js
+++ banner/js/main.js
@@ -1,14 +1,12 @@
 function addElements() {
-    var a = document.getElementById("hosting");
-    var m = new Date().getMonth() + 1;
+    var imgHost = $("img#hosting");
+    const month = new Date().getMonth() + 1;
 
-    if (m == 12 || m == 1) {
-        a.src="img/dobojcaffe.png";
-        a.alt="Web hosting by DobojCaffe.com";
-        a.className="img-fluid mx-auto d-block";
+    if (month == 12 || month == 1) {
+        imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
+        imgHost.addClass("img-fluid mx-auto d-block");
     } else {
-        a.src="img/docaffe.jpg";
-        a.alt="Web hosting by DobojCaffe.com";
-        a.className="img-fluid mx-auto d-block";
+        imgHost.attr({"src":"img/docaffe.jpg", "alt":"Web hosting by DobojCaffe.com"});
+        imgHost.addClass("img-fluid mx-auto d-block");
     }
 }
