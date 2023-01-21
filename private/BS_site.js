869d44564235a2627876b4b2c340186597d43b28
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2023-01-21 10:21:34 +0100

    Update BS_site.js

--- private/BS_site.js
+++ private/BS_site.js
@@ -5,7 +5,7 @@ var year = new Date().getFullYear();
 
 $("head").prepend("<base href="+siteURL+">");
 $("head").append("<meta name='copyright' content='DobojCaffe.com & Servis računara &quot;BALEŠEVIĆ&quot; &copy;"+year+"'>");
-$("head").append("<meta name='GENERATOR' content='Visual Studio Code 1.74.2'>");
+$("head").append("<meta name='GENERATOR' content='Visual Studio Code 1.74.3'>");
 $("#godina").text(year);
 
 var imgHost = $("img#hosting");
