9071be18cbaae5756e0dc062e7f3cd7f89ca5a25
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2023-04-12 13:28:38 +0200

    Update main.js

--- banner/js/main.js
+++ banner/js/main.js
@@ -1,12 +1,22 @@
 function addElements() {
     var imgHost = $("img#hosting");
-    const month = new Date().getMonth() + 1;
+    //const month = new Date().getMonth() + 1;
 
-    if (month == 12 || month == 1) {
+    switch (new Date().getMonth()+1) {
+        case (1 || 12):
+            imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
+            imgHost.addClass("img-fluid mx-auto d-block");
+            break;
+        default:
+            imgHost.attr({"src":"img/docaffe.jpg", "alt":"Web hosting by DobojCaffe.com"});
+            imgHost.addClass("img-fluid mx-auto d-block");
+    }
+    
+    /* if (month == 12 || month == 1) {
         imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
         imgHost.addClass("img-fluid mx-auto d-block");
     } else {
         imgHost.attr({"src":"img/docaffe.jpg", "alt":"Web hosting by DobojCaffe.com"});
         imgHost.addClass("img-fluid mx-auto d-block");
-    }
+    } */
 }
