f7fadbedec3d30ba622dca9f4e68f0ed70f49206
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2023-04-12 11:25:56 +0200

    Update BS_CP_main.js

--- private/BS_CP_main.js
+++ private/BS_CP_main.js
@@ -11,18 +11,18 @@ $("head").append("<meta http-equiv='refresh' content='1800'>");
 }); */
 
 function login() {
-    var a = $("#username").css("text-transform", "lowercase");
-    var b = $(":password").css("text-transform", "lowercase");
+    var a = $("#username");
+    var b = $(":password");
     const user = { username: "djidjimilovich", password: "baleshevich88" };
 
-    if (a.val() == "" && b.val() == "") {
+    if (a.val() === "" && b.val() === "") {
         alert("Unos nije dozvoljen.\nMorate popuniti sva polja da biste nastavili dalje.");
-    } else if (user.username != a.val()) {
+    } else if (user.username !== a.val()) {
         a.val("");
         console.error(DateTimeLocal, ": Unijeli ste pogrešno korisničko ime. Pokušajte ponovo.");
         alert("Unijeli ste pogrešno korisničko ime.");
         a.focus();
-    } else if (user.password != b.val()) {
+    } else if (user.password !== b.val()) {
         b.val("");
         console.error(DateTimeLocal, ": Unijeli ste pogrešnu lozinku. Pokušajte ponovo.");
         alert("Unijeli ste pogrešnu lozinku.");
@@ -43,18 +43,29 @@ function loadRadioStation() {
 function IzracunajNoviNovcaniIznos() {
     var h = $("#novcaniIznos").val();
     var k = $("#procenatIznos").val();
-    var l = $("#vrstaObracunaNovca").val();
+    //var l = $("#vrstaObracunaNovca").val();
     var procenat = k / 100;
     var procenat2 = h * procenat;
 
-    if (l == "umanjenjeIznos") {
+    switch ($("#vrstaObracunaNovca").val()) {
+        case "umanjenjeIznos":
+            var rezUmanjenje = h - procenat2;
+            $("#NoviNovcaniIznos").text("Novi novčani iznos umanjenje - " + rezUmanjenje + " KM.");
+            break;
+        case "uvecanjeIznos":
+            var rezUvecanje = +h + +procenat2;
+            $("#NoviNovcaniIznos").text("Novi novčani iznos uvećanje - " + rezUvecanje + " KM.");
+    }
+
+    /* if (l == "umanjenjeIznos") {
         var rezUmanjenje = h - procenat2;
         $("#NoviNovcaniIznos").text("Novi novčani iznos umanjenje - " + rezUmanjenje + " KM.");
     } else if (l == "uvecanjeIznos") {
         var rezUvecanje = +h + +procenat2;
         $("#NoviNovcaniIznos").text("Novi novčani iznos uvećanje - " + rezUvecanje + " KM.");
     }
-    return true;
+    return true; */
+
 }
 
 function IzracunajDividendu() {
