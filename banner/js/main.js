c5aeb8e0c9402e073ceb9668c2fc278cf87ca86c
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2023-12-05 13:43:03 +0100

    Update main.js

--- banner/js/main.js
+++ banner/js/main.js
@@ -2,17 +2,15 @@ function addElements() {
     var imgHost = $("img#hosting");
     //const month = new Date().getMonth() + 1;
 
-    switch (new Date().getMonth()+1) {
+    switch (new Date().getMonth() + 1) {
         case 1:
         case 12:
-            imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
-            imgHost.addClass("img-fluid mx-auto d-block");
+            imgHost.attr({ "src": "img/dobojcaffe.png", "alt": "Web hosting by DobojCaffe.com" }).addClass("img-fluid mx-auto d-block");
             break;
         default:
-            imgHost.attr({"src":"img/docaffe.jpg", "alt":"Web hosting by DobojCaffe.com"});
-            imgHost.addClass("img-fluid mx-auto d-block");
+            imgHost.attr({ "src": "img/docaffe.jpg", "alt": "Web hosting by DobojCaffe.com" }).addClass("img-fluid mx-auto d-block");
     }
-    
+
     /* if (month == 12 || month == 1) {
         imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
         imgHost.addClass("img-fluid mx-auto d-block");
