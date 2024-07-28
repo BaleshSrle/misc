49293f5b4e1f5501f4bdfcee738e899e06549449
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2024-07-28 14:09:14 +0200

    Update BS_main.js

--- private/BS_main.js
+++ private/BS_main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     $("head").append("<meta name='MSThemeCompatible' content='yes'>", "<meta http-equiv='cleartype' content='on'>", "<meta http-equiv='x-dns-prefetch-control' content='on'>", "<meta http-equiv='Cache-control' content='no-cache'>", "<meta http-equiv='Pragma' content='no-cache'>", "<meta http-equiv='Expires' content='-1'>");
     $("head").prepend("<link rel='dns-prefetch' href='https://cdn.jsdelivr.net/'>", "<link rel='dns-prefetch' href='https://fonts.gstatic.com'>", "<link rel='preconnect' href='https://cdn.jsdelivr.net/'>", "<link rel='preconnect' href='https://fonts.googleapis.com'>", "<link rel='preconnect' href='https://fonts.gstatic.com'>", "<link rel='prefetch' href='https://img.shields.io/'>", "<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Ubuntu&display=swap' media='all'>", "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css' media='all'>");
     $("head").append("<link rel='icon' href='https://cdn.jsdelivr.net/gh/BaleshSrle/baleshsrle.github.io/images/logo.png'>");
-    $("[href$=bootstrap.min.css],[src$=jquery.min.js],[href$=bootstrap.bundle.min.js]").on("error", function () {
+    $("[href$='bootstrap.min.css'],[src$='jquery.min.js'],[href$='bootstrap.bundle.min.js']").on("error", function () {
         location.reload();
     });
     $("[rel='preconnect'],[href$='.min.css'],[src$='.min.js']").attr("crossorigin", "anonymous");
