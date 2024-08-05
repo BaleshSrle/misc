003e168769d92eb89d470c81609dd04f2d94b362
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2024-08-05 16:39:20 +0200

    Create BS_DeviceLook.js

---
+++ private/BS_DeviceLook.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $("body").css("background", "#f1f1f1");
+    $("form").attr("method", "post").css({ "text-align": "center", "padding": "16px 0px" });
+    $("select#url").on("click", function () {
+        $("iframe#urlOutput").attr("src", $("#url").val());
+    });
+    $("div.laptop").css({ "-webkit-transform-origin": "0 0", "transform-origin": "0 0", "-webkit-transform": "scale(.6) translate(-50%)", "transform": "scale(.6) translate(-50%)", "left": "50%", "position": "absolute", "width": "1366px", "height": "800px", "border-width": "24px 24px 80px", "border-radius": "6px" });
+    $("div.laptop").children().css({ "width": "1366px", "height": "800px", "overflow": "hidden", "border": "0 none" });
+    $("div.smartphone").css({ "width": "360px", "height": "640px" });
+    $("div.smartphone").children().css({ "width": "360px", "height": "640px" });
+    $("div.tablet").css({ "width": "768px", "height": "1024px" });
+    $("div.tablet").children().css({ "width": "768px", "height": "1024px", "margin": "-1px" });
+    $("div").filter(".laptop,.smartphone,.tablet").css("border", "black-solid");
+    $("div").filter(".laptop,.smartphone,.tablet").children().css("background", "white");
+    $("div").filter(".smartphone,.tablet").css({ "position": "relative", "margin": "auto", "border-width": "60px 16px", "border-radius": "36px" });
+});
