0e226f5420877109e0e776db42b2bc702f038e79
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2024-09-05 08:43:51 +0200

    Update BS_main.js

--- private/BS_main.js
+++ private/BS_main.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
         $("html").attr("lang", "sr-BA");
         $("head").prepend("<meta name='robots' content='index, nofollow'>", "<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
     }
+    if (location.pathname == "/private/kontrolna_tabla.html" || location.pathname =="/D:/PC-Admin/Documents/private/kontrolna_tabla.html") {
+        $.getScript("https://cdn.jsdelivr.net/gh/BaleshSrle/misc/private/BS_CP_main.min.js");
+    } else {
+        $.getScript("https://cdn.jsdelivr.net/gh/BaleshSrle/misc/private/BS_site.min.js");
+    }
     
     $("head").prepend("<meta name='author' content='Baleševi&#263; Sr&#273;an, srdjan.b%40teol.net'>", "<meta name='designer' content='Servis računara &quot;BALEŠEVIĆ&quot;'>", "<meta name='reply-to' content='baleshevichcompany%40dobojcaffe.com'>", "<meta name='language' content='sr'>", "<meta name='host' content='www.dobojcaffe.com'>", "<meta name='geo.region' content='BA-SRP'>", "<meta name='geo.placename' content='Добој'>", "<meta name='twitter:creator' content='@BaleshSrle'>", "<meta name='twitter:site' content='@BaleshSrle'>");
     $("head").prepend("<meta name='MSThemeCompatible' content='yes'>", "<meta http-equiv='cleartype' content='on'>", "<meta http-equiv='x-dns-prefetch-control' content='on'>", "<meta http-equiv='Cache-control' content='no-cache'>", "<meta http-equiv='Pragma' content='no-cache'>", "<meta http-equiv='Expires' content='-1'>");
