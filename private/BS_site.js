358fcfd28e556ef1b795b94425c707bf0a289c69
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2025-08-17 23:49:51 +0200

    Update BS_site.js

--- private/BS_site.js
+++ private/BS_site.js
@@ -87,7 +87,7 @@ jQuery(function ($) {
         //$(this).filter("#microsoft,#linux,#internet").find("img").css("max-width", "180px").addClass("d-block mx-auto h-auto");
         //$(this).filter("#MediaPlayer,#WebBrowser,#microsoft,#linux,#internet").find("carousel-item:first-child").addClass("active");
         $(this).filter("#MinOfInteriorSrpskaCyber").children("div.card-body").wrapInner($("<a></a>").attr({ "href": "https://mup.vladars.rs/vtk-lat/", "hreflang": "sr-BA", "target": "_blank" }).append($("<img>").attr({ "src": "images/vtk.png", "alt": "MUP R. Srpske - Uprava krim. policije - Jedinica za opšti kriminalitet - Odjeljenje za visokotehnološki kriminalitet" }).addClass("d-block mx-auto img-fluid").css("width", "180")));
-        $(this).filter("#speedtest_banner").children("div.card-body").append($("<object></object>").attr({ "data": "https://img.shields.io/badge/Speedtest%20by%20Ookla-141526?style=for-the-badge&logo=speedtest&logoSize=auto&link=https%3A%2F%2Fspeedtest.net", "name": "Provjerite brzinu svog Interneta" }).addClass("rounded-lg d-block mx-auto img-fluid"));
+        $(this).filter("#speedtest_banner").children("div.card-body").append($("<object></object>").attr({ "data-label1": "Speedtest by Ookla", "data-color1": "141526", "data-badgestyle": "for-the-badge", "data-logo": "speedtest", "data-logosize": "auto", "data-link": "https://speedtest.net", "data-name": "Provjerite brzinu svog Interneta" }).addClass("StaticBadge rounded-lg d-block mx-auto img-fluid"));
     });
     //$("div.col-sm-6.col-xl-8").addClass("my-2 px-md-2");
     $("article").each(function () {
