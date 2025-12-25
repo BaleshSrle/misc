3824bfac93d351abd1c36da361351656d87278ce
Baleshevich Srdjan <srdjan.balesevic@yahoo.com>
2025-12-25 17:39:15 +0100

    Updating winter and non-winter banner & implementing moment.js
    Updating README.md, refactoring & transfering extra code from HTML to JS, refactoring JS file and implementing Moment.js . All external Boopstrap, JQuery and moment.js files now have failsafe via cdnjs.

--- README.md
+++ README.md
@@ -7,7 +7,10 @@
 Miscellaneous projects on which I'm working on.
 
 ## ABP
-<img src="https://img.shields.io/badge/Adblock%20Plus-c70d2c?logo=adblockplus&logoColor=white&logoSize=auto&labelColor=555555" alt="Adblock Plus" style="vertical-align: middle;"> Filter which I have created and I'm also maintaining it.
+<img src="https://img.shields.io/badge/Adblock%20Plus-c70d2c?logo=adblockplus&logoColor=white&logoSize=auto&labelColor=555555" alt="Adblock Plus" style="vertical-align: middle;"> filter which I have created and I'm also maintaining it.
+
+---
 
 ## Banner
-This project is displaying winter holiday banner or common banner on a web-site, depending on current month.
+This project is displaying winter holiday banner or common banner on a web-site, depending on current date.
+This project is created using ![Bootstrap Badge](https://img.shields.io/github/v/release/twbs/bootstrap?filter=v4.*&logo=bootstrap&logoColor=white&label=Bootstrap&color=7952b3) & ![jQuery Badge](https://img.shields.io/github/v/release/jquery/jquery?filter=3.*&logo=jquery&logoColor=white&label=jQuery&color=0769ad) via ![jsDelivr](https://img.shields.io/badge/jsDelivr-e84d3d?logo=jsdelivr&logoColor=white&labelColor=555555) CDN (with [cdnjs](https://cdnjs.com) as failsafe) & incorporates ![Moment.js Badge](https://img.shields.io/github/v/tag/moment/moment?filter=2.*&label=Moment.js&color=61b2a7).
--- banner/js/main.js
+++ banner/js/main.js
@@ -1,21 +1,18 @@
-function addElements() {
-    var imgHost = $("img#hosting");
-    //const month = new Date().getMonth() + 1;
-
-    switch (new Date().getMonth() + 1) {
-        case 1:
-        case 12:
-            imgHost.attr({ "src": "img/dobojcaffe.png", "alt": "Web hosting by DobojCaffe.com" }).addClass("img-fluid mx-auto d-block");
-            break;
-        default:
-            imgHost.attr({ "src": "img/docaffe.jpg", "alt": "Web hosting by DobojCaffe.com" }).addClass("img-fluid mx-auto d-block");
-    }
-
-    /* if (month == 12 || month == 1) {
-        imgHost.attr({"src":"img/dobojcaffe.png", "alt":"Web hosting by DobojCaffe.com"});
-        imgHost.addClass("img-fluid mx-auto d-block");
-    } else {
-        imgHost.attr({"src":"img/docaffe.jpg", "alt":"Web hosting by DobojCaffe.com"});
-        imgHost.addClass("img-fluid mx-auto d-block");
-    } */
-}
+$(document).ready(() => {
+    $("link").on("error", function() {
+        $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.csss");
+    });
+    $("script:eq(1)").on("error", function() {
+        $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js");
+    });
+    $("script:eq(2)").on("error", function() {
+        $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js");
+    });
+    $("body").addClass("bg-dark text-white");
+    $("main").addClass("container");
+    $("div.card").addClass("bg-dark w-25 mx-auto mt-5 border-secondary");
+    $("div.card-header").addClass("border-bottom border-secondary").children("h6").addClass("mb-0");
+    (moment().isBetween("2025-12-02", "2026-01-16")) ? $("img#hosting").attr("src","img/dobojcaffe.png") : $("img#hosting").attr("src","img/docaffe.jpg");
+    $("img#hosting").addClass("img-fluid d-block mx-auto").attr({"alt": "Web hosting by DobojCaffe.com", "loading": "lazy"}).wrap($("<a></a>").attr({"href": "https://www.dobojcaffe.com", "hreflang": "sr-YU", "target": "_blank", "rel": "alternate"}));
+    document.normalize();
+});
