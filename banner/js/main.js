962b2672038594667f0f64995337855de4d3fdbb
Baleshevich Srdjan <srdjan.balesevic@yahoo.com>
2026-01-06 17:12:44 +0100

    Refacroring the moment.js & adding snowfall into the page

--- banner/js/main.js
+++ banner/js/main.js
@@ -1,18 +1,22 @@
 $(document).ready(() => {
-    $("link").on("error", function() {
+
+    $("link").on("error", function () {
         $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.csss");
     });
-    $("script:eq(1)").on("error", function() {
+    $("script:eq(1)").on("error", function () {
         $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js");
     });
-    $("script:eq(2)").on("error", function() {
+    $("script:eq(2)").on("error", function () {
         $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js");
+    });
+    $("script:eq(3)").on("error", function () {
+        $(this).attr("src", "https://cdnjs.cloudflare.com/ajax/libs/JQuery-Snowfall/1.7.4/snowfall.jquery.min.js");
     });
     $("body").addClass("bg-dark text-white");
     $("main").addClass("container");
     $("div.card").addClass("bg-dark w-25 mx-auto mt-5 border-secondary");
     $("div.card-header").addClass("border-bottom border-secondary").children("h6").addClass("mb-0");
-    (moment().isBetween("2025-12-02", "2026-01-16")) ? $("img#hosting").attr("src","img/dobojcaffe.png") : $("img#hosting").attr("src","img/docaffe.jpg");
-    $("img#hosting").addClass("img-fluid d-block mx-auto").attr({"alt": "Web hosting by DobojCaffe.com", "loading": "lazy"}).wrap($("<a></a>").attr({"href": "https://www.dobojcaffe.com", "hreflang": "sr-YU", "target": "_blank", "rel": "alternate"}));
+    (moment().isBetween("2026-01-16", "2026-12-02")) ? $("img#hosting").attr("src", "img/docaffe.jpg") : ($("img#hosting").attr("src", "img/dobojcaffe.png"), $(document).snowfall({flakeColor: "white", minSpeed: 2, maxSpeed: 10, round: false, shadow: false }));
+    $("img#hosting").addClass("img-fluid d-block mx-auto").attr({ "alt": "Web hosting by DobojCaffe.com", "loading": "lazy" }).wrap($("<a></a>").attr({ "href": "https://www.dobojcaffe.com", "hreflang": "sr-YU", "target": "_blank", "rel": "alternate" }));
     document.normalize();
 });
